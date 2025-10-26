41a7894681c0c889f115d4e8c13a389a8287c790
LuceroEstrada <52812@LuceroEstrada.(none)>
2025-10-25 18:27:28 -0600

    modificacion diseño interfaz

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,13 @@
-import './App.css'
+import Errores from './Errores'
+import LogIn from './LogIn'
+import InfoEmpleado from './InfoEmpleado'
 
 function App() {
   return (
-    <div className="container">
-      <div className="top-bar">
-        <p className="count-text">Conteo: 2/10</p>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="icon-check">
-          <path
-            fillRule="evenodd"
-            d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </div>
-
-      <div className="image-container">
-        <img
-          className="image-s"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsB8NKORD4j_knDUWeGVucC3wxIgXBlMu0Pw&s"
-          alt="Vista cámara"
-        />
-      </div>
-
-      <div className="error-message">
-        <p>Falta 1 paquete</p>
-      </div>
+    <div>
+      <LogIn />
+      <Errores />
+      <InfoEmpleado />
     </div>
   )
 }
---
+++ src/LogIn.jsx
@@ -0,0 +1,53 @@
+function LogIn() {
+  return (
+    <div className="flex items-center justify-center w-full min-h-screen bg-white dark:bg-slate-900">
+      <div className="flex flex-col w-full max-w-md p-8 rounded-2xl bg-white shadow-lg dark:bg-slate-800">
+        {/* Título */}
+        <h2 className="text-4xl font-bold text-center text-gray-800 dark:text-white leading-tight">
+            <span className="block">Welcome to</span>
+            <span className="block text-2xl font-bold text-gray-800">Gate Group</span>
+        </h2>
+
+        {/* Formulario */}
+        <form className="flex flex-col gap-5">
+          {/* Campo: correo */}
+          <div>
+            <label className="block text-sm font-medium text-slate-950 dark:text-gray-300 mb-1">
+              Usuario
+            </label>
+            <input
+              type="text"
+              placeholder="Ingresa tu usuario"
+              className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:gray dark:bg-slate-700 dark:text-white"
+              required
+            />
+          </div>
+
+          {/* Campo: contraseña */}
+          <div>
+            <label className="block text-sm font-medium text-slate-950 dark:text-gray-300 mb-1">
+              Contraseña
+            </label>
+            <input
+              type="password"
+              placeholder="Ingresa tu contraseña"
+              className="w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:gray dark:bg-slate-950 dark:text-white"
+              required
+            />
+          </div>
+
+          {/* Botón */}
+          <button
+            type="submit"
+            className="mt-4 w-full py-2 bg-slate-950 text-white font-semibold rounded-lg hover:bg-white hover:text-black transition-all"
+          >
+            Entrar
+          </button>
+        </form>
+
+      </div>
+    </div>
+  );
+}
+
+export default LogIn;
