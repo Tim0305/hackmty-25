196179ba7ac8d0ebfec13087eca973c17d501276
Timoteo <tabtim1015@gmail.com>
2025-10-26 01:19:28 -0600

    Feat: Productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import Employees from "./pages/Empleados"
+import Employees from "./pages/Employees"
 import LogIn from "./pages/LogIn"
 import Manager from "./pages/Manager"
+import Products from "./pages/Products"
 
 function App() {
   return (
     <div>
-      <Employees />
+      <Manager />
     </div>
   )
 }
