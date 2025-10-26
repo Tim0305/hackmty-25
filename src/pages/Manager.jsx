e1df926eb62aa4de530060a685ba6a32a4dd8f8e
Timoteo <tabtim1015@gmail.com>
2025-10-25 21:30:38 -0600

    Feat: Manager errores

--- src/pages/Manager.jsx
+++ src/pages/Manager.jsx
@@ -45,39 +45,58 @@ export default function Manager() {
           </IconButton>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-10 p-10">
-          <Card className="shadow-lg w-fit self-start">
-            <CardContent className="flex flex-col items-center p-4">
-              <Typography variant="h6" gutterBottom>
-                Bandejas hechas hoy
-              </Typography>
-              <Typography variant="h2" className="text-blue-600 !font-bold">
-                42
-              </Typography>
-            </CardContent>
-          </Card>
+        <div className="flex flex-col pr-10 pb-10 pl-10">
+          <h1 className="text-orange-500 text-6xl font-bold mb-15 ml-1 md:ml-0">
+            Bienvenido!
+          </h1>
 
-          <Card className="shadow-lg flex-1">
-            <CardContent className="h-[500px]">
-              <Typography variant="h6" gutterBottom>
-                Errores por día
-              </Typography>
-              <ResponsiveContainer width="100%" height="90%">
-                <LineChart data={data}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="day" />
-                  <YAxis />
-                  <Tooltip />
-                  <Line
-                    type="monotone"
-                    dataKey="errors"
-                    stroke="#3b82f6"
-                    strokeWidth={3}
-                  />
-                </LineChart>
-              </ResponsiveContainer>
-            </CardContent>
-          </Card>
+          <div className="flex flex-col md:flex-row gap-10">
+            <div className="flex flex-col gap-10">
+              <Card className="shadow-lg w-full">
+                <CardContent className="flex flex-col items-center p-4">
+                  <Typography variant="h6" gutterBottom>
+                    Bandejas hechas hoy
+                  </Typography>
+                  <Typography variant="h2" className="text-blue-600 !font-bold">
+                    42
+                  </Typography>
+                </CardContent>
+              </Card>
+
+              <Card className="shadow-lg w-full">
+                <CardContent className="flex flex-col items-center p-4">
+                  <Typography variant="h6" gutterBottom>
+                    Errores hoy
+                  </Typography>
+                  <Typography variant="h2" className="text-red-600 !font-bold">
+                    15
+                  </Typography>
+                </CardContent>
+              </Card>
+            </div>
+
+            <Card className="shadow-lg flex-1">
+              <CardContent className="h-[500px]">
+                <Typography variant="h6" gutterBottom>
+                  Errores por día
+                </Typography>
+                <ResponsiveContainer width="100%" height="90%">
+                  <LineChart data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="day" />
+                    <YAxis />
+                    <Tooltip />
+                    <Line
+                      type="monotone"
+                      dataKey="errors"
+                      stroke="#3b82f6"
+                      strokeWidth={3}
+                    />
+                  </LineChart>
+                </ResponsiveContainer>
+              </CardContent>
+            </Card>
+          </div>
         </div>
       </main>
     </div>
